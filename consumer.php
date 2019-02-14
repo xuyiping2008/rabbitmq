e5d5e22e62b1d9c40d1acc8a2e9d93e9ccee5b1e
xuyiping <15001829801@163.com>
2019-02-14 10:40:19 +0800

    test

--- consumer.php
+++ consumer.php
@@ -31,13 +31,13 @@ $ex = new AMQPExchange($channel);
 $ex->setName($e_name);
 $ex->setType(AMQP_EX_TYPE_DIRECT); //direct类型
 $ex->setFlags(AMQP_DURABLE); //持久化
-echo "Exchange Status:".$ex->declare()."\n";
+//echo "Exchange Status:".$ex->declare()."\n";
 
 //创建队列
 $q = new AMQPQueue($channel);
 $q->setName($q_name);
 $q->setFlags(AMQP_DURABLE); //持久化
-echo "Message Total:".$q->declare()."\n";
+//echo "Message Total:".$q->declare()."\n";
 
 //绑定交换机与队列，并指定路由键
 echo 'Queue Bind: '.$q->bind($e_name, $k_route)."\n";
