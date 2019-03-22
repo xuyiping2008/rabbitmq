8fd26833a7026820f64c413c4705bdfe4c123bb0
xuyiping <15001829801@163.com>
2019-03-22 15:11:46 +0800

    test

--- producer.php
+++ producer.php
@@ -18,21 +18,14 @@ $channel = new AMQPChannel($conn);
 
 $e_name = 'e_linvo'; //交换机名
 $k_route = 'user';
-/*$k_route1 = 'user.save'; //路由key
-$k_route2 = 'user.update'; //路由key
-$k_route3 = 'user.delete.abc'; //路由key*/
 //创建交换机
 $ex = new AMQPExchange($channel);
 $ex->setName($e_name);
-//$ex->setType(AMQP_EX_TYPE_DIRECT); //direct类型
-$ex->setType(AMQP_EX_TYPE_TOPIC); //direct类型
-//$ex->setFlags(AMQP_DURABLE); //持久化
+$ex->setType(AMQP_EX_TYPE_DIRECT); //direct类型
+$ex->setFlags(AMQP_DURABLE); //持久化
 date_default_timezone_set("Asia/Shanghai");
 
 $message = "Hello world!";
-/*echo "Send Message:".$ex->publish($message , $k_route1)."\n";
-echo "Send Message:".$ex->publish($message , $k_route2)."\n";
-echo "Send Message:".$ex->publish($message , $k_route3)."\n";*/
 
 for($i=0; $i<5; ++$i){
     $message = "Hello world!".$i;
